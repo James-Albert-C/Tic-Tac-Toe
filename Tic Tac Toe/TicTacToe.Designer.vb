8be84000cc9c15c6e001758234ac1ce00d2e0d9b
James Albert <51251053+James-Albert-C@users.noreply.github.com>
2019-07-02 15:29:56 -0400

    Add files via upload

---
+++ Tic Tac Toe/TicTacToe.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TicTacToe
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TicTacToe))
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.A3 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.A2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.C2 = New System.Windows.Forms.Button()
+        Me.C1 = New System.Windows.Forms.Button()
+        Me.C3 = New System.Windows.Forms.Button()
+        Me.A1 = New System.Windows.Forms.Button()
+        Me.fileStrip = New System.Windows.Forms.MenuStrip()
+        Me.FilrToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewGameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.fileStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'B1
+        '
+        Me.B1.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.B1, "B1")
+        Me.B1.Name = "B1"
+        Me.B1.UseVisualStyleBackColor = False
+        '
+        'A3
+        '
+        Me.A3.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.A3, "A3")
+        Me.A3.Name = "A3"
+        Me.A3.UseVisualStyleBackColor = False
+        '
+        'B2
+        '
+        Me.B2.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.B2, "B2")
+        Me.B2.Name = "B2"
+        Me.B2.UseVisualStyleBackColor = False
+        '
+        'A2
+        '
+        Me.A2.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.A2, "A2")
+        Me.A2.Name = "A2"
+        Me.A2.UseVisualStyleBackColor = False
+        '
+        'B3
+        '
+        Me.B3.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.B3, "B3")
+        Me.B3.Name = "B3"
+        Me.B3.UseVisualStyleBackColor = False
+        '
+        'C2
+        '
+        Me.C2.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.C2, "C2")
+        Me.C2.Name = "C2"
+        Me.C2.UseVisualStyleBackColor = False
+        '
+        'C1
+        '
+        Me.C1.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.C1, "C1")
+        Me.C1.Name = "C1"
+        Me.C1.UseVisualStyleBackColor = False
+        '
+        'C3
+        '
+        Me.C3.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.C3, "C3")
+        Me.C3.Name = "C3"
+        Me.C3.UseVisualStyleBackColor = False
+        '
+        'A1
+        '
+        Me.A1.BackColor = System.Drawing.Color.LightSteelBlue
+        resources.ApplyResources(Me.A1, "A1")
+        Me.A1.Name = "A1"
+        Me.A1.UseVisualStyleBackColor = False
+        '
+        'fileStrip
+        '
+        Me.fileStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FilrToolStripMenuItem})
+        resources.ApplyResources(Me.fileStrip, "fileStrip")
+        Me.fileStrip.Name = "fileStrip"
+        '
+        'FilrToolStripMenuItem
+        '
+        Me.FilrToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewGameToolStripMenuItem, Me.CloseToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.FilrToolStripMenuItem.Name = "FilrToolStripMenuItem"
+        resources.ApplyResources(Me.FilrToolStripMenuItem, "FilrToolStripMenuItem")
+        '
+        'NewGameToolStripMenuItem
+        '
+        Me.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem"
+        resources.ApplyResources(Me.NewGameToolStripMenuItem, "NewGameToolStripMenuItem")
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        resources.ApplyResources(Me.CloseToolStripMenuItem, "CloseToolStripMenuItem")
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        resources.ApplyResources(Me.AboutToolStripMenuItem, "AboutToolStripMenuItem")
+        '
+        'TicTacToe
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.C3)
+        Me.Controls.Add(Me.A1)
+        Me.Controls.Add(Me.C1)
+        Me.Controls.Add(Me.C2)
+        Me.Controls.Add(Me.A2)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.A3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.fileStrip)
+        Me.MainMenuStrip = Me.fileStrip
+        Me.Name = "TicTacToe"
+        Me.ShowIcon = False
+        Me.fileStrip.ResumeLayout(False)
+        Me.fileStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents B1 As Button
+    Friend WithEvents A3 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents A2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents C2 As Button
+    Friend WithEvents C1 As Button
+    Friend WithEvents C3 As Button
+    Friend WithEvents A1 As Button
+    Friend WithEvents fileStrip As MenuStrip
+    Friend WithEvents FilrToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewGameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Tic Tac Toe/TicTacToe.vb
@@ -0,0 +1,154 @@
+﻿'Programer: James Clyburn
+'Date: 6/14/19
+
+Option Strict On
+Public Class TicTacToe
+    'Constants
+    Const winTitle As String = "Winner"
+
+    'Declarations
+    Dim personTurn As Boolean = False
+    Dim turnNumber As Integer = 0
+    Dim winFound As Boolean = False
+    Private buttons As List(Of Button)
+
+    Private Sub TicTacToe_Load(sender As Object, e As EventArgs) Handles Me.Load
+        buttons = New List(Of Button) From {A1, A2, A3, B1, B2, B3, C1, C2, C3}
+    End Sub
+
+    Private Sub A1_Click(sender As Object, e As EventArgs) Handles A1.Click
+        letterChooser(A1)
+        winnerChecker()
+    End Sub
+
+    Private Sub A2_Click(sender As Object, e As EventArgs) Handles A2.Click
+        letterChooser(A2)
+        winnerChecker()
+    End Sub
+
+    Private Sub A3_Click(sender As Object, e As EventArgs) Handles A3.Click
+        letterChooser(A3)
+        winnerChecker()
+    End Sub
+
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click
+        letterChooser(B1)
+        winnerChecker()
+    End Sub
+
+    Private Sub B2_Click(sender As Object, e As EventArgs) Handles B2.Click
+        letterChooser(B2)
+        winnerChecker()
+    End Sub
+
+    Private Sub B3_Click(sender As Object, e As EventArgs) Handles B3.Click
+        letterChooser(B3)
+        winnerChecker()
+    End Sub
+
+    Private Sub C1_Click(sender As Object, e As EventArgs) Handles C1.Click
+        letterChooser(C1)
+        winnerChecker()
+    End Sub
+
+    Private Sub C2_Click(sender As Object, e As EventArgs) Handles C2.Click
+        letterChooser(C2)
+        winnerChecker()
+    End Sub
+
+    Private Sub C3_Click(sender As Object, e As EventArgs) Handles C3.Click
+        letterChooser(C3)
+        winnerChecker()
+    End Sub
+
+    Private Sub letterChooser(location As Button)
+        If personTurn = False Then
+            location.Text = "X"
+        Else
+            location.Text = "O"
+        End If
+        location.Enabled = False
+        turnNumber += 1
+        personTurn = Not personTurn
+    End Sub
+
+    Private Sub winnerChecker()
+        If A1.Text = A2.Text And A1.Text = A3.Text And Not A1.Text = "" Then
+            winFound = True
+            If A1.Text = "X" Then
+                MsgBox("X wins!", , winTitle)
+            Else
+                MsgBox("O wins!", , winTitle)
+            End If
+        End If
+
+        If B1.Text = B2.Text And B1.Text = B3.Text And Not B1.Text = "" Then
+            winFound = True
+            If B1.Text = "X" Then
+                MsgBox("X wins!", , winTitle)
+            Else
+                MsgBox("O wins!", , winTitle)
+            End If
+        End If
+
+        If C1.Text = C2.Text And C1.Text = C3.Text And Not C1.Text = "" Then
+            winFound = True
+            If C1.Text = "X" Then
+                MsgBox("X wins!", , winTitle)
+            Else
+                MsgBox("O wins!", , winTitle)
+            End If
+        End If
+
+        If A1.Text = B2.Text And A1.Text = C3.Text And Not A1.Text = "" Then
+            winFound = True
+            If A1.Text = "X" Then
+                MsgBox("X Wins", , winTitle)
+            Else
+                MsgBox("O wins!", , winTitle)
+            End If
+        End If
+
+        If A3.Text = B2.Text And A3.Text = C1.Text And Not A3.Text = "" Then
+            winFound = True
+            If A3.Text = "X" Then
+                MsgBox("X Wins", , winTitle)
+            Else
+                MsgBox("O wins!", , winTitle)
+            End If
+        End If
+
+        If winFound = True Then
+            For index = 0 To buttons.Count - 1
+                buttons(index).Enabled = False
+            Next
+        End If
+
+        If winFound = False And turnNumber = 9 Then
+            MsgBox("Cat!", , winTitle)
+        End If
+    End Sub
+
+    Private Sub initializer()
+        personTurn = False
+        turnNumber = 0
+        winFound = False
+
+        For index = 0 To buttons.Count - 1
+            buttons(index).Enabled = True
+            buttons(index).Text = ""
+        Next
+    End Sub
+
+    Private Sub NewGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewGameToolStripMenuItem.Click
+        initializer()
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MsgBox("This game was made by James Clyburn.", , "Creator")
+    End Sub
+End Class
